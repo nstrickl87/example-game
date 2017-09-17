4a51465d034aa09d38a083403b10ece0abf7ee0a
kilvaari <nstrickland@outlook.com>
2017-09-16 21:09:00 -0700

    vdvv
    vdvd

---
+++ Unity-Project/Assets/Editor/HelloWorld.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEditor;
+public class HelloWorld
+{
+    [MenuItem("Tools/Create HelloWorld")]
+    private static void CreateHelloWorldGameObject()
+    {
+        if (EditorUtility.DisplayDialog(
+        "Hello World",
+        "Do you really want to do this?",
+        "Create",
+        "Cancel"))
+        {
+            new GameObject("HelloWorld");
+        }
+    }
+}
